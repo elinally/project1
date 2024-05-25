cc99e5e66f4d184d4a5cd566a1e796b9aa360daf
elina11pr <119628554+elina11pr@users.noreply.github.com>
2024-05-26 01:44:39 +0300

    Connect MongoDB to project

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import mongoose from "mongoose"; 
 
 const app = express();
 
-app.use(express.json());
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.error(err);
+  });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
